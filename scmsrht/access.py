0f7163c226c89d56c35c8472b54890ba64cb4e3d
Ludovic Chabant <ludovic@chabant.com>
2019-01-21 08:16:03 +0000

    Allow using access functions outside of flask apps

--- scmsrht/access.py
+++ scmsrht/access.py
@@ -12,12 +12,12 @@ class UserAccess(IntFlag):
     write = 2
     manage = 4
 
-def check_repo(user, repo, authorized=current_user):
-    User = current_app.User
+def check_repo(user, repo, authorized=current_user, tables=current_app):
+    User = tables.User
     u = User.query.filter(User.username == user).first()
     if not u:
         abort(404)
-    Repository = current_app.Repository
+    Repository = tables.Repository
     _repo = Repository.query.filter(Repository.owner_id == u.id)\
             .filter(Repository.name == repo).first()
     if not _repo:
@@ -27,18 +27,18 @@ def check_repo(user, repo, authorized=current_user):
             abort(404)
     return u, _repo
 
-def get_repo(owner_name, repo_name):
+def get_repo(owner_name, repo_name, tables=current_app):
     if owner_name[0] == "~":
-        User = current_app.User
+        User = tables.User
         user = User.query.filter(User.username == owner_name[1:]).first()
         if user:
-            Repository = current_app.Repository
+            Repository = tables.Repository
             repo = Repository.query.filter(Repository.owner_id == user.id)\
                 .filter(Repository.name == repo_name).first()
         else:
             repo = None
         if user and not repo:
-            Redirect = current_app.Redirect
+            Redirect = tables.Redirect
             repo = (Redirect.query
                     .filter(Redirect.owner_id == user.id)
                     .filter(Redirect.name == repo_name)
@@ -48,13 +48,13 @@ def get_repo(owner_name, repo_name):
         # TODO: organizations
         return None, None
 
-def get_repo_or_redir(owner, repo):
+def get_repo_or_redir(owner, repo, tables=current_app):
     owner, repo = get_repo(owner, repo)
     if not repo:
         abort(404)
     if not has_access(repo, UserAccess.read):
         abort(401)
-    if isinstance(repo, current_app.Redirect):
+    if isinstance(repo, tables.Redirect):
         view_args = request.view_args
         if not "repo" in view_args or not "owner" in view_args:
             return redirect(url_for(".summary",
@@ -65,12 +65,12 @@ def get_repo_or_redir(owner, repo):
         abort(redirect(url_for(request.endpoint, **view_args)))
     return owner, repo
 
-def get_access(repo, user=None):
+def get_access(repo, user=None, tables=current_app):
     if not user:
         user = current_user
     if not repo:
         return UserAccess.none
-    if isinstance(repo, current_app.Redirect):
+    if isinstance(repo, tables.Redirect):
         # Just pretend they have full access for long enough to do the redirect
         return UserAccess.read | UserAccess.write | UserAccess.manage
     if not user:
@@ -80,8 +80,8 @@ def get_access(repo, user=None):
         return UserAccess.none
     if repo.owner_id == user.id:
         return UserAccess.read | UserAccess.write | UserAccess.manage
-    acl = current_app.Access.query.filter(
-            current_app.Access.repo_id == repo.id).first()
+    acl = tables.Access.query.filter(
+            tables.Access.repo_id == repo.id).first()
     if acl:
         acl.updated = datetime.utcnow()
         db.session.commit()
@@ -93,14 +93,14 @@ def get_access(repo, user=None):
         return UserAccess.none
     return UserAccess.read
 
-def has_access(repo, access, user=None):
-    return access in get_access(repo, user)
+def has_access(repo, access, user=None, tables=current_app):
+    return access in get_access(repo, user, tables=tables)
 
-def check_access(owner_name, repo_name, access):
+def check_access(owner_name, repo_name, access, tables=current_app):
     owner, repo = get_repo(owner_name, repo_name)
     if not owner or not repo:
         abort(404)
-    a = get_access(repo)
+    a = get_access(repo, tables=tables)
     if not UserAccess.write in a:
         abort(404)
     if not access in a:
